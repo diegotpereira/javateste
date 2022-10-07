41240c18b0a025b0a5f689586fa7a165930cc2f0
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-10-07 14:25:47 -0300

    criação do app java para teste com junit.

--- src/App.java
+++ src/App.java
@@ -1,5 +1,25 @@
 public class App {
+
+    public static int adicionar(int primeiroNumero, int segundoNumero) {
+
+        return primeiroNumero + segundoNumero;
+    }
+
+    public static int multiplicar(int multiplicando, int multiplicador) {
+
+        return multiplicando * multiplicador;
+    }
+
+    public static int dividir(int dividendo, int divisor) {
+
+        if(divisor == 0)
+
+           throw new IllegalArgumentException("Não pode dividir poor zero");
+
+        return dividendo/divisor;
+    }
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        System.out.println(App.adicionar(3, 3));
     }
 }
--- src/AppTeste.java
+++ src/AppTeste.java
@@ -1,12 +1,54 @@
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Assert;
 import org.junit.Test;
 
 
 public class AppTeste {
 
     @Test
-    public void teste() {
+    public void deveriaResponderComVerdadeiro() {
 
-        assertEquals(2, 1 + 1);
+        assertTrue(true);
+    }
+
+    @Test
+    public void adicionar_Dois_Mais_Dois_Retornar_Quatro() {
+
+        // arranjo
+        final int experado = 4;
+
+        // ação
+        final int atual = App.adicionar(2, 2);
+
+        // assert
+        Assert.assertEquals(experado, atual);
+    }
+
+    @Test
+    public void multiplicarQuatroVezesDois_Retorna_Oito() {
+
+        // arranjo
+        final int experado = 8;
+
+        // ação
+        final int atual = App.multiplicar(4, 2);
+
+        // assert 
+        Assert.assertEquals(experado, atual);
+    }
+
+    @Test
+    public void dividir_Dez_Dividido_Dois_Retorna_Cinco() {
+
+        //arranjo
+        final int experado = 5;
+
+        // ação 
+        final int atual = App.dividir(10, 2);
+
+        // 
+        Assert.assertEquals(experado, atual);
     }
 }

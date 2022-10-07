ee5857f51deb0bb6a81e7ec4240f7b2f91e7061b
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-10-07 14:07:27 -0300

    first commit

---
+++ src/AppTeste.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+
+public class AppTeste {
+
+    @Test
+    public void teste() {
+
+        assertEquals(2, 1 + 1);
+    }
+}
